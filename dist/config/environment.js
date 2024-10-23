c2c236a3182824ec0e3c7293b981918ea6a2ba0e
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2024-10-23 10:45:04 +0530

    dotenv hide

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -1,6 +1,6 @@
 const environment = {
-    SUPABASE_URL: process.env.SUPABASE_URL || 'https://qsmreturqldqiwoudjdi.supabase.co',
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbXJldHVycWxkcWl3b3VkamRpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTU5MDg0MiwiZXhwIjoyMDQ1MTY2ODQyfQ.B_lRvcxxXzwjBjflcMNw5XOBD6e3E6XqKm1iymwMmE8',
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
 };
 export default environment;
 //# sourceMappingURL=environment.js.map
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,8 +6,8 @@ interface Environment {
   }
   
   const environment: Environment = {
-    SUPABASE_URL: process.env.SUPABASE_URL || 'https://qsmreturqldqiwoudjdi.supabase.co',
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbXJldHVycWxkcWl3b3VkamRpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTU5MDg0MiwiZXhwIjoyMDQ1MTY2ODQyfQ.B_lRvcxxXzwjBjflcMNw5XOBD6e3E6XqKm1iymwMmE8',
+    SUPABASE_URL: process.env.SUPABASE_URL!,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!
   };
   
   export default environment;
